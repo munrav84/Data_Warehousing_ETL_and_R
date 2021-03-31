125c55eb8627caca89e43287f6a1ce470fdf9da6
munrav84 <33266493+munrav84@users.noreply.github.com>
2021-03-30 21:54:58 -0400

    Update DimensionTableCreation.sql

--- DimensionTableCreation.sql
+++ DimensionTableCreation.sql
@@ -1,17 +1,17 @@
-CREATE TABLE [Insert Schema Name].[Insert Area Table Name](
+CREATE TABLE [CRIME].[AREA](
 	[Area Name ID] [int] IDENTITY (1,1) NOT NULL,
 	[Area Name] [varchar](50) NULL,
 	[StateID] [int] NULL,
 	[State] [varchar](50) NULL
 
 
-CREATE TABLE [Insert Schema Name].[Insert State Table Name](
+CREATE TABLE [CRIME].[STATE](
 	[StateID] [int] IDENTITY (1,1) NOT NULL,
 	[State] [varchar](50) NULL
 
 
 
-CREATE TABLE [Insert Schema Name].[Insert Code Table Name](
+CREATE TABLE [CRIME].[CODE](
 	[Code ID] [int] IDENTITY (1,1) NOT NULL,
 	[Rural_urban_continuum_code_2003] [char](1) NULL,
 	[Urban_influence_code_2003] [char] (1) NULL,
